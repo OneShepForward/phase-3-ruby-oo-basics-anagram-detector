4a93289cf0460a972402f6ded967aeb32c91b3ca
Nicholas Shephard <shephardn@gmail.com>
2021-12-15 06:55:54 -0600

    Yay, I did it!

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+    attr_accessor :chars
+
+    def initialize(word)
+        @word = word
+        @sorted_chars = word.chars.sort
+    end
+
+    def match(array_of_words)
+        matches = []
+        array_of_words.each do |word|
+            if word.chars.sort == @sorted_chars
+                matches << word
+            else
+                puts "nope"
+            end
+        end
+        matches
+    end
+end
+
+# binding.pry
